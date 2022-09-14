56e4885c435d64b8f60cbc67fe8256adca66c70b
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2022-09-13 22:15:35 -0300

    add client streaming

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"github.com/luisbilecki/grpc-fullcycle/pb"
 	"google.golang.org/grpc"
@@ -19,7 +20,8 @@ func main() {
 
 	client := pb.NewUserServiceClient(connection)
 	// AddUser(client)
-	AddUserVerbose(client)
+	// AddUserVerbose(client)
+	AddUsers(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -59,4 +61,46 @@ func AddUserVerbose(client pb.UserServiceClient) {
 		}
 		fmt.Println("Status:", stream.Status, " - ", stream.GetUser())
 	}
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "l1",
+			Name:  "Luis 1",
+			Email: "luis1@luis.com",
+		},
+		&pb.User{
+			Id:    "l2",
+			Name:  "Luis 2",
+			Email: "luis2@luis.com",
+		},
+		&pb.User{
+			Id:    "l3",
+			Name:  "Luis 3",
+			Email: "luis3@luis.com",
+		},
+		&pb.User{
+			Id:    "l4",
+			Name:  "Luis 4",
+			Email: "luis4@luis.com",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error receiving response: %v", err)
+	}
+
+	fmt.Println(res)
 }
